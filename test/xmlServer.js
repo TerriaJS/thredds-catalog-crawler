669f1cfdcacf6cc91db88050590da60b00ef0d60
Nanda <nanda.sankaran@gmail.com>
2024-05-28 11:43:16 +1000

    Use import.meta.url to build dirname.

--- test/xmlServer.js
+++ test/xmlServer.js
@@ -3,11 +3,13 @@ import listen from 'test-listen'
 import express from 'express'
 import serveStatic from 'serve-static'
 import path from 'path'
+import { fileURLToPath } from 'url';
 
 export async function startServer () {
-    var app = express();
+    const app = express();
     const server = http.createServer(app);
-    app.use(serveStatic(path.join(import.meta.dirname)))
+    const dirname = path.dirname(fileURLToPath(import.meta.url));
+    app.use(serveStatic(dirname))
     const url = await listen(server);
     return { toString: () => url, stop: () => server.close() };
 }
